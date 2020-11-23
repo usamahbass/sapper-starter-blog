4635d9d7e5887c3171d73d2373985617989d7700
usamahbass <basalamahusamah7@gmail.com>
2020-11-24 06:55:48 +0700

    add desc in md and loop post

--- src/post/example.md
+++ src/post/example.md
@@ -1,6 +1,7 @@
 ---
 title: "Example Post"
 date: "2020-11-24"
+desc: "Example post is blablabla"
 ---
 
 Example Post
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,14 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+  import { Posts } from "../posts.js";
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<h1>Blog Starter</h1>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+{#each Posts as post}
+  <article>
+    <a href="/posts/{post.link}">
+      <h3>{post.title}</h3>
+      <p>{post.desc}</p>
+    </a>
+  </article>
+{/each}
